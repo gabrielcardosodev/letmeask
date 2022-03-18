dea4200befd239be6ae31cfa8120d350f8358e63
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-03-18 19:31:15 -0300

    feat(pages): add home page to project
    home page is already styled with syled components

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,36 @@
+import Button from '../../components/Button/Button'
+
+import illustrationImg from '../../assets/images/illustration.svg'
+import logoImg from '../../assets/images/logo.svg'
+import googleIconImg from '../../assets/images/google-icon.svg'
+
+import { Container } from './styles'
+
+export function Home() {
+  return(
+    <Container>
+        <aside>
+            <img src={illustrationImg} alt="Ilustração de perguntas e respostas" />
+            <strong>Toda pergunta tem uma resposta.</strong>
+            <p>Aprenda a compartilhar conhecimento com outras pessoas.</p>
+        </aside>
+        <main>
+            <div className='main-content'>
+                <img src={logoImg} alt="Logo da Letmeask" />
+                <button>
+                    <img src={googleIconImg} alt="Logo do Google" />
+                    Cria sua sala com o Google
+                </button>
+                <div className='separator'>ou entre em uma sala</div>
+                <form>
+                    <input 
+                        type="text"
+                        placeholder='Digite o código da sala'
+                    />
+                    <Button type="submit" />
+                </form>
+            </div>
+        </main>
+    </Container>
+  )
+}
