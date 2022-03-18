fb6b0d3bf4110c04f98b20dde986f01fca38195e
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-03-18 19:37:58 -0300

    feat(components): add Button component
    this button will used many times in project and it's better to create one component to make be easy
    in the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { Home } from "./pages/Home/Home";
+import { GlobalStyles } from './styles/global'
+
 export function App() {
-  return <h1>Hello World</h1>
-}
+  return (
+    <>
+    <Home />
+    <GlobalStyles />
+    </>
+  )
+} 
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,15 @@
+import { ButtonHTMLAttributes } from "react";
+import { ButtonStyles } from "./styles";
+
+import loginIconImg from '../../assets/images/login.svg'
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+
+export default function Button(props: ButtonProps) {
+  return (
+    <ButtonStyles {...props}>
+        <img src={loginIconImg} alt="Ícone de login" />
+        Entrar em uma sala
+    </ButtonStyles>
+  )
+}
