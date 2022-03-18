e357915eaf0b4f0273e24a0c146595ef5001d7f0
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-03-18 13:43:24 -0300

    refactor(services): move firebase sensitive data to .env.local file

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDceV40HN5i5gF_VKohu-gzEl18NUan8zw",
-  authDomain: "letmeask-fdd44.firebaseapp.com",
-  databaseURL: "https://letmeask-fdd44-default-rtdb.firebaseio.com",
-  projectId: "letmeask-fdd44",
-  storageBucket: "letmeask-fdd44.appspot.com",
-  messagingSenderId: "524676943631",
-  appId: "1:524676943631:web:4d79ff99a39084a409f87a"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
