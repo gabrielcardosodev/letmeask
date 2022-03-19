f213b9f7cb275eae39fa300b162ba8bbbf615b4b
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-03-19 18:19:14 -0300

    feat(pages): add NewRoom pages to project
    NewRoom page is already styled

---
+++ src/pages/NewRoom/NewRoom.tsx
@@ -0,0 +1,34 @@
+import Button from '../../components/Button/Button'
+
+import illustrationImg from '../../assets/images/illustration.svg'
+import logoImg from '../../assets/images/logo.svg'
+
+
+import { Container } from './styles'
+export function NewRoom () {
+    return (
+        <Container>
+        <aside>
+            <img src={illustrationImg} alt="Ilustração de perguntas e respostas" />
+            <strong>Toda pergunta tem uma resposta.</strong>
+            <p>Aprenda a compartilhar conhecimento com outras pessoas.</p>
+        </aside>
+        <main>
+            <div className='main-content'>
+                <img src={logoImg} alt="Logo da Letmeask" />
+                <h2>Crie uma nova sala</h2>
+                <form>
+                    <input 
+                        type="text"
+                        placeholder='Nome da sala'
+                    />
+                    <Button type='submit'>
+                        Criar sala
+                    </Button>
+                </form>
+                <p>Quer entrar em uma sala já existente? <a href="#">Clique aqui</a></p>
+            </div>
+        </main>
+    </Container>
+    )
+}
