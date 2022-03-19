db4e929eccd32b244bcbed27c6ddb2ee7f46e5b6
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-03-19 19:05:08 -0300

    feat(pages): add possibility to move to home page
    add possibility to move to home page clicking in footer link

--- src/pages/NewRoom/NewRoom.tsx
+++ src/pages/NewRoom/NewRoom.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import Button from '../../components/Button/Button'
 
 import illustrationImg from '../../assets/images/illustration.svg'
@@ -26,7 +28,7 @@ export function NewRoom () {
                         Criar sala
                     </Button>
                 </form>
-                <p>Quer entrar em uma sala já existente? <a href="#">Clique aqui</a></p>
+                <p>Quer entrar em uma sala já existente? <Link to='/'>Clique aqui</Link></p>
             </div>
         </main>
     </Container>
