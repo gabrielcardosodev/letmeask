b101cb37e39ff0ceed3a086563043e1613db0fbf
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-03-19 18:58:35 -0300

    feat(app): add routes to App
    add routes to app to move to home and NewRoom pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route, } from 'react-router-dom'
+
 import { Home } from "./pages/Home/Home";
+import { NewRoom } from './pages/NewRoom/NewRoom'
+
 import { GlobalStyles } from './styles/global'
 
 export function App() {
   return (
     <>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/rooms/new' element={<NewRoom />} />
+      </Routes>
+    </BrowserRouter>
     <GlobalStyles />
     </>
   )
