e244b2baa8f0980435f649db73ec446755f9352d
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-03-21 20:15:43 -0300

    refactor(components): create a component to reuse Aside in others components

---
+++ src/components/Aside/Aside.tsx
@@ -0,0 +1,13 @@
+import illustrationImg from '../../assets/images/illustration.svg'
+
+import { Container } from './styles'
+
+export function Aside() {
+    return (
+        <Container>
+            <img src={illustrationImg} alt="Ilustração de perguntas e respostas" />
+            <strong>Toda pergunta tem uma resposta.</strong>
+            <p>Aprenda a compartilhar conhecimento com outras pessoas.</p>
+        </Container>
+    )
+}
---
+++ src/components/Aside/styles.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+export const Container = styled.aside`
+        display: flex;
+        flex: 1;
+        justify-content: center;
+        flex-direction: column;
+
+        background: var(--purple);
+        color: #FFFFFF;
+        
+
+        padding: 8rem 5rem;
+
+        img {
+            max-width: 320px;
+            width: 100%;
+        }
+
+        strong {
+            margin-top: 1rem;
+            font: 700 2rem 'Poppins', sans-serif;
+            line-height: 3rem;
+        }
+
+        p {
+            margin-top: 1rem;
+            font-size: 1.6rem;
+            line-height: 2.1rem;
+            color: var(--background);
+        }
+`
+
+
