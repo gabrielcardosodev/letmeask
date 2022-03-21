89d25bddc3bad8b10a9744b913b02c19bc7b2d4b
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-03-21 20:18:43 -0300

    refactor(pages): use Aside and ContentNewRoom components

--- src/pages/NewRoom/NewRoom.tsx
+++ src/pages/NewRoom/NewRoom.tsx
@@ -1,39 +1,19 @@
-import { useAuth } from '../../hooks/useAuth'
-import { Link } from 'react-router-dom'
+import { Aside } from "../../components/Aside/Aside"
+import { ContentNewRoom } from "../../components/CreateNewRoom/ContentNewRoom"
 
-import Button from '../../components/Button/Button'
+import styled from "styled-components"
 
-import illustrationImg from '../../assets/images/illustration.svg'
-import logoImg from '../../assets/images/logo.svg'
-
-import { Container } from './styles'
+export const Container = styled.div`
+    display: flex;
+    align-items: stretch;
+    height: 100vh;
+`
 
 export function NewRoom () {
-    const { user } = useAuth()
-
     return (
         <Container>
-        <aside>
-            <img src={illustrationImg} alt="Ilustração de perguntas e respostas" />
-            <strong>Toda pergunta tem uma resposta.</strong>
-            <p>Aprenda a compartilhar conhecimento com outras pessoas.</p>
-        </aside>
-        <main>
-            <div className='main-content'>
-                <img src={logoImg} alt="Logo da Letmeask" />
-                <h2>Crie uma nova sala</h2>
-                <form>
-                    <input 
-                        type="text"
-                        placeholder='Nome da sala'
-                    />
-                    <Button type='submit'>
-                        Criar sala
-                    </Button>
-                </form>
-                <p>Quer entrar em uma sala já existente? <Link to='/'>Clique aqui</Link></p>
-            </div>
-        </main>
-    </Container>
+            <Aside />
+            <ContentNewRoom />
+        </Container>    
     )
 }
