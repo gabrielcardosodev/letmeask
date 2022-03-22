a9d8c3d05bd167f1e07ef01588df081c757b7d6f
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-03-21 21:41:47 -0300

    feat(pages): use Aside and HomePageContent

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,49 +1,27 @@
-import { ButtonHTMLAttributes } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useAuth } from '../../hooks/useAuth'
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
 
-import { Button } from '../../components/Button/index'
+import { Aside } from '../../components/Aside/index';
+import { ContentHomePage } from '../../components/HomePageContent';
 
-import logoImg from '../../assets/images/logo.svg'
-import googleIconImg from '../../assets/images/google-icon.svg'
-
-import { Container } from './styles'
-import { Aside } from '../../components/Aside/index'
+import { Container } from '../../styles/global';
 
 export function Home() {
-    const navigate = useNavigate();
-    const { user, signInWithGoogle } = useAuth()
+  const navigate = useNavigate();
+  const { user, signInWithGoogle } = useAuth();
 
-    async function handleCreateRoom() {
-         if (!user) {
-            await signInWithGoogle()
-        }
-
-        navigate("/rooms/new");
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
     }
 
-  return(
+    navigate('/rooms/new');
+  }
+
+  return (
     <Container>
-        <Aside />
-        <main>
-            <div className='main-content'>
-                <img src={logoImg} alt="Logo da Letmeask" />
-                <button onClick={handleCreateRoom}>
-                    <img src={googleIconImg} alt="Logo do Google" />
-                    Cria sua sala com o Google
-                </button>
-                <div className='separator'>ou entre em uma sala</div>
-                <form>
-                    <input 
-                        type="text"
-                        placeholder='Digite o código da sala'
-                    />
-                    <Button 
-                          type="submit"
-                    />
-                </form>
-            </div>
-        </main>
+      <Aside />
+      <ContentHomePage handleCreateRoom={handleCreateRoom} />
     </Container>
-  )
+  );
 }
