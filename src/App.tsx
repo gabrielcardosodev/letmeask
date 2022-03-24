aff0277d40bd1753fedc62737ecd29d6a1fad612
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-03-24 19:49:02 -0300

    feat(routes): add room page route

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { NewRoom } from './pages/NewRoom/index';
 import { AuthContextProvider } from './contexts/AuthContext';
 
 import { GlobalStyles } from './styles/global';
+import { Room } from './pages/Room';
 
 export function App() {
   return (
@@ -14,6 +15,7 @@ export function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/rooms/new" element={<NewRoom />} />
+            <Route path="/rooms/:id" element={<Room />} />
           </Routes>
         </AuthContextProvider>
       </BrowserRouter>
