5b972371a2eca349c90287f986e2ac8766eca31d
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-03-25 17:59:33 -0300

    feat(page): add message error with react-hot-toast

--- src/components/HomePageContent/index.tsx
+++ src/components/HomePageContent/index.tsx
@@ -1,5 +1,6 @@
 import { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import toast, { Toaster } from 'react-hot-toast';
 import { useAuth } from '../../hooks/useAuth';
 
 import { ButtonStyles } from '../../UI/Button/styles';
@@ -35,34 +36,37 @@ export function ContentHomePage() {
     const roomRef = await database.ref(`rooms/${roomCode}`).get();
 
     if (!roomRef.exists()) {
-      alert('Room does not exists.');
+      toast.error('Room does not exists.');
       return;
     }
 
     navigate(`/rooms/${roomCode}`);
   }
   return (
-    <Content>
-      <div className="main-content">
-        <img src={logoImg} alt="Logo da Letmeask" />
-        <button onClick={handleCreateRoom}>
-          <img src={googleIconImg} alt="Logo do Google" />
-          Cria sua sala com o Google
-        </button>
-        <div className="separator">ou entre em uma sala</div>
-        <form onSubmit={handleJoinRoom}>
-          <input
-            type="text"
-            placeholder="Digite o código da sala"
-            onChange={(e) => setRoomCode(e.target.value)}
-            value={roomCode}
-          />
-          <ButtonStyles type="submit">
-            <img src={loginIconImg} alt="ícone de login" />
-            Entrar em uma sala
-          </ButtonStyles>
-        </form>
-      </div>
-    </Content>
+    <>
+      <Toaster position="top-right" reverseOrder={true} />
+      <Content>
+        <div className="main-content">
+          <img src={logoImg} alt="Logo da Letmeask" />
+          <button onClick={handleCreateRoom}>
+            <img src={googleIconImg} alt="Logo do Google" />
+            Cria sua sala com o Google
+          </button>
+          <div className="separator">ou entre em uma sala</div>
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+              onChange={(e) => setRoomCode(e.target.value)}
+              value={roomCode}
+            />
+            <ButtonStyles type="submit">
+              <img src={loginIconImg} alt="ícone de login" />
+              Entrar em uma sala
+            </ButtonStyles>
+          </form>
+        </div>
+      </Content>
+    </>
   );
 }
